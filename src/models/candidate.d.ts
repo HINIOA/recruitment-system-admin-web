e2bcf98263210ba7a341ba4d4bffaf56b0e21fcb
hinioa <1271889901@qq.com>
2021-02-04 13:05:14 +0800

    feat: 添加候选人类型定义文件

---
+++ src/models/candidate.d.ts
@@ -0,0 +1,14 @@
+export interface Candidate {
+  id: string;
+  name: string;
+  job: string;
+  education: string;
+  sex: string;
+  age: number;
+  phone: string;
+  email: string;
+  time: Date;
+  step: string;
+  status: string;
+  resumeUrl: string;
+}

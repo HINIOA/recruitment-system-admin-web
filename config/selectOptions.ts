3f1991fca3ed594625212991d0187f248585325c
hinioa <1271889901@qq.com>
2021-02-15 20:20:31 +0800

    refactor: 抽离选择框选项到配置文件

---
+++ config/selectOptions.ts
@@ -0,0 +1,56 @@
+export default {
+  department: [
+    {
+      value: 'A部门',
+      label: 'A部门',
+    },
+    {
+      value: 'B部门',
+      label: 'B部门',
+    },
+    {
+      value: 'C部门',
+      label: 'C部门',
+    },
+    {
+      value: 'D部门',
+      label: 'D部门',
+    },
+  ],
+  type: [
+    {
+      value: '研发类',
+      label: '研发类',
+    },
+    {
+      value: '职能类',
+      label: '职能类',
+    },
+    {
+      value: '全职',
+      label: '全职',
+    },
+    {
+      value: '实习',
+      label: '实习',
+    },
+  ],
+  location: [
+    {
+      value: '广东·深圳市',
+      label: '广东·深圳市',
+    },
+    {
+      value: '北京市',
+      label: '北京市',
+    },
+    {
+      value: '上海市',
+      label: '上海市',
+    },
+    {
+      value: '广东·广州市',
+      label: '广东·广州市',
+    },
+  ],
+};
--- src/pages/Jobs/components/SearchSelector.tsx
+++ src/pages/Jobs/components/SearchSelector.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Select } from 'antd';
+import selectOptions from '../../../../config/selectOptions';
 
 const SearchSelector: React.FC<{
   state: {
@@ -20,44 +21,12 @@ const SearchSelector: React.FC<{
   >([]);
 
   useEffect(() => {
-    if (type === 'types')
-      setOptions([
-        {
-          label: '全职',
-          value: '全职',
-        },
-        {
-          label: '实习',
-          value: '实习',
-        },
-        {
-          label: '研发类',
-          value: '研发类',
-        },
-        {
-          label: '职能类',
-          value: '职能类',
-        },
-      ]);
-    else
-      setOptions([
-        {
-          label: 'A部门',
-          value: 'A部门',
-        },
-        {
-          label: 'B部门',
-          value: 'B部门',
-        },
-        {
-          label: 'C部门',
-          value: 'C部门',
-        },
-        {
-          label: 'D部门',
-          value: 'D部门',
-        },
-      ]);
+    const options: {
+      value: string;
+      label: string;
+    }[] = type === 'types' ? selectOptions.type : selectOptions.department;
+
+    setOptions(options);
   }, [JSON.stringify(state)]);
 
   return (
